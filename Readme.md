1f2e1b119ea143362f55e8f16b9561a47dd1465e
valer <gmo230374@spengergasse.at>
2026-03-05 18:12:30 +0100

    finished application

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# instructions to use the applications
+* use browser to open http://localhost.8080/start
---
+++ src/main/java/at/spengergasse/spring_thymeleaf/entities/Lehrer.java
@@ -0,0 +1,55 @@
+package at.spengergasse.spring_thymeleaf.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Lehrer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String name;
+    private String fach;
+    private LocalDate birthDate;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFach() {
+        return fach;
+    }
+    public void setFach(String fach) {
+        this.fach = fach;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
